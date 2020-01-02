8c97951ee95f5e64538bcb911a5aca588c24ed45
zbjlala <zhangbaoj@chanjet.com>
2020-01-02 13:48:09 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"go-gin/pkg/setting"
+	"go-gin/routers"
+	"net/http"
+)
+
+func main() {
+	//router := gin.Default()
+	//router.GET("/test", func(context *gin.Context) {
+	//	context.JSON(http.StatusOK, gin.H{
+	//		"message": "test",
+	//	})
+	//})
+
+
+	//endless.DefaultReadTimeOut = setting.ReadTimeout
+	//endless.DefaultWriteTimeOut = setting.WriteTimeout
+	//endless.DefaultMaxHeaderBytes = 1 << 20
+	//endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	//
+	//server := endless.NewServer(endPoint, routers.InitRouter())
+	//server.BeforeBegin = func(add string) {
+	//	log.Println("Actual pid is %d", syscall.Getpid())
+	//}
+	//
+	//err := server.ListenAndServe()
+	//if err != nil{
+	//	log.Printf("Server err: %v", err)
+	//}
+
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:	fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:	router,
+		ReadTimeout:	setting.ReadTimeout,
+		WriteTimeout:	setting.WriteTimeout,
+		MaxHeaderBytes:	1 << 20,
+	}
+
+	s.ListenAndServe()
+}
---
+++ routers/router.go
@@ -0,0 +1,40 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-gin/middleware/jwt"
+	"go-gin/pkg/setting"
+	"go-gin/routers/api"
+	v1 "go-gin/routers/api/v1"
+)
+
+func InitRouter() *gin.Engine{
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.Recovery())
+
+	gin.SetMode(setting.RunMode)
+
+	//r.GET("/test", func(context *gin.Context) {
+	//	context.JSON(http.StatusOK, gin.H{
+	//		"message": "test",
+	//	})
+	//})
+	r.GET("/auth", api.GetAuth)
+
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
+	{
+		//获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		//新建标签
+		apiv1.POST("/tags", v1.AddTag)
+		//更新指定标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//删除指定标签
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	}
+	return r
+}
